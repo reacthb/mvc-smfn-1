ade646ec18f00ed8b0f4519386ec2c5f1ebbcce0
hcnbnl <hcnbnl@free.fr>
2019-08-28 11:26:07 +0200

    first commit

--- templates/view/errorView.php
+++
@@ -1,4 +0,0 @@
-<div>
-	<h1><?php echo $message; ?></h1>
-</div>
-<a href='/'>Back to home</a>
--- templates/view/footer.php
+++
@@ -1,6 +0,0 @@
-        <div>
-            <a href='/'>Back to home</a>
-        </div>
-        <script type="text/javascript" src="/js/main.js"></script>
-    </body>
-</html>
